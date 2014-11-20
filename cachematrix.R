95ede9f6568fdce335a30be7542a979039c6e78a
Paule Kodjou <pkodjou@cisco.com>
2014-11-19 20:32:25 -0800

    Some cleanup
    Cleanup

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,22 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
 ## Write a short comment describing this function
 
+## This function is checking if the matrix x has changed
+## since the inverse of x was created. 
+## inv_mat is the inversed matrix
+## cache_mat is the backup of matrix x that was done last 
+## time that we did the inversion and calculated the inv_mat
+## When it returns TRUE, it means that we need to recalculate the inverse
+## When it returns FALSE it means that we can just get the inverse from 
+## the cache
+#
 makeCacheMatrix <- function(x = matrix(), cache_mat) 
 {
-        ## This function is checking if the matrix x has changed
-        ## since the inverse of x was created. inv_mat is the inverse matrix
-        ## cache_mat is the backup of matrix x that was done last 
-        ## time that we did the inversion and calculated the inv_mat
-        ## identity is the identity matrix 
-        
         calc <- FALSE
         message ("printing cache_mat")
         print (cache_mat)
+
         if ((identical (x, cache_mat)) == FALSE)
         {
                 calc <- TRUE
@@ -25,20 +28,18 @@ makeCacheMatrix <- function(x = matrix(), cache_mat)
 
 ## Write a short comment describing this function
 
+## This function returns a matrix (inv_mat) that is the inverse of 'x'
+## determine if we need to invert the matrix
+## or get it from cache?
+
 cacheSolve <- function(x = matrix ()) 
-{
-       
-        ## Return a matrix that is the inverse of 'x'
-        ## determine if we need to invert the matrix
-        ## or get it from cache?
-        
-        
+{      
         if (!exists ("inv_mat"))
         {
                 ## x has not yet been inverted. inv_mat does not exist
                 ## Doing the matrix inversion
                 
-                message ("Recalculating the inverted matrix (inv_mat)")
+                message ("Calculating the inverted matrix (inv_mat) for the first time")
                 inv_mat <- solve (x) ## generate inv_mat       
         }
         else
@@ -60,20 +61,28 @@ cacheSolve <- function(x = matrix ())
                 message ("x has changed. Recalculating the inverted matrix (inv_mat)")        
                         
                 inv_mat <- solve (x) ## generate inv_mat       
-                cache_mat <<- x ## Save input matrix in a variable        
                 }
         }
 
-        cache_mat <<- x ## Save input matrix in a variable
+        ## Save input matrix (x) in cache_mat so we can use it next time
+        ## to check to see if we should get the inverse from the cache
+        
+        cache_mat <<- x 
         message ("printing cache_mat at the end of the main function")
         print (cache_mat)
         
-        ### creates an identity matrix for x - will be used to
+        message ("printing inv_mat at the end of the main function")
+        print (inv_mat)
+        
+        ### creates an identity matrix for x. It will be used to
         ### confirm that the inversion was done correctly
         
         mat_row <- nrow (x)
         my_identity <<- diag (mat_row) 
 
+        message ("printing the identity matrix at the end of the main function")
+        print (my_identity)
+        
         return (inv_mat)
 }
 
